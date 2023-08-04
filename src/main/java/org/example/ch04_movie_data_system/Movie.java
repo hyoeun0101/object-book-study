8db44fb1707daee763fc8474e6fa66feb7a8a034
hyoeun <kimhyoeun0101@gmail.com>
2023-08-04 22:23:26 +0900

    영화클래스를 데이터 중심으로 설계하기

---
+++ src/main/java/org/example/ch04_movie_data_system/Movie.java
@@ -0,0 +1,31 @@
+package org.example.ch04_movie_data_system;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Duration;
+@Getter @Setter
+public class Movie {
+    private String title;
+    private Duration runningTime;
+    private Money fee;
+
+    /**
+     * 할인 조건 목록을 직접 가지고 있다.
+     */
+    private List<DiscountCondition> discountConditions;
+
+    /**
+     * 할인 정책의 종류
+     */
+    private MovieType movieType;
+
+    /**
+     * 할인금액을 직접 가지고 있다.
+     */
+    private Money discountAmount;
+    /**
+     * 할인 비율을 직접 가지고 있다.
+     */
+    private double discountPercent;
+}
